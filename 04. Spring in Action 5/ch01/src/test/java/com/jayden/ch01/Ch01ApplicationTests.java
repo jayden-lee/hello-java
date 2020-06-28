83b65a921f2250671c59130fc990318c2924dc0e
jayden-lee <gwanggi.lee@gmail.com>
2020-06-29 00:01:12 +0900

    Create ch01 example for spring in action 5

---
+++ 04. Spring in Action 5/ch01/src/test/java/com/jayden/ch01/Ch01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayden.ch01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
