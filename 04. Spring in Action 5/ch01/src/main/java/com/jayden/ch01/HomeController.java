ac57ef63ff66df0419f88cea2b6e4231907e52d8
jayden-lee <gwanggi.lee@gmail.com>
2020-06-29 00:34:54 +0900

    Add ch01example for spring in action 5

---
+++ 04. Spring in Action 5/ch01/src/main/java/com/jayden/ch01/HomeController.java
@@ -0,0 +1,13 @@
+package com.jayden.ch01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
