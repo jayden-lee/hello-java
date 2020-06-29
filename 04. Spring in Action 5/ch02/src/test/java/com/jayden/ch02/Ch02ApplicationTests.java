2da1e593016e1c26b59da308388bcd060f0dfa24
jayden-lee <gwanggi.lee@gmail.com>
2020-06-29 15:44:43 +0900

    Create ch02 example for spring in action 5

---
+++ 04. Spring in Action 5/ch02/src/test/java/com/jayden/ch02/Ch02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayden.ch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
